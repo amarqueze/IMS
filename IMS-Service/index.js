4a5b3e0ea5714b77258d171986dac8caa4f9feb5
Alan M.E <alanmarquez@outlook.com>
2018-09-30 19:58:54 -0500

    controllers binded

--- IMS-Service/index.js
+++ IMS-Service/index.js
@@ -1,11 +1,33 @@
+/*
+Copyright 2018 Alan M.E
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 var server = require("./app/server/server");
 // logger libreria
 
 /* import controllers */
 var UsersController = require('./app/controllers/userscontroller');
+var ProductsController = require('./app/controllers/productscontroller');
+var ProvidersController = require('./app/controllers/providerscontroller');
 
 var dispatcher = server();
-dispatcher.bind_controller("/users", UsersController);
+dispatcher
+    .bind_controller("/users", UsersController)
+    .bind_controller("/products", ProductsController)
+    .bind_controller("/providers", ProvidersController);
+
 
 dispatcher.run(80, function(port) {
     console.log(`Server listening port: ${port}`);
