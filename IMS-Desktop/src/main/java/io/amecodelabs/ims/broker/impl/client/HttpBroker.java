a35a52d270015cad27fd091979c961b9d54b1605
Alan M.E <alanmarquez@outlook.com>
2018-10-28 16:53:24 -0500

    HttpBroker-client single instance

--- IMS-Desktop/src/main/java/io/amecodelabs/ims/broker/impl/client/HttpBroker.java
+++ IMS-Desktop/src/main/java/io/amecodelabs/ims/broker/impl/client/HttpBroker.java
@@ -3,9 +3,11 @@ package io.amecodelabs.ims.broker.impl.client;
 import io.amecodelabs.ims.broker.client.HttpConnect;
 
 public class HttpBroker {
+	private static HttpConnect connect;
 	
 	public static HttpConnect getHttpConnect() {
-		return new DefaultHttpConnect();
+		if(connect == null) connect = new DefaultHttpConnect();
+		return connect;
 	}
 	
 }
