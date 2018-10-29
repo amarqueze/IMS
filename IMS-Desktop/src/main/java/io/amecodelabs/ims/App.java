ed4735f2f83aa29c2df9f4473d290b817071ce11
Alan M.E <alanmarquez@outlook.com>
2018-10-28 19:25:28 -0500

    implemented launcher

--- IMS-Desktop/src/main/java/io/amecodelabs/ims/App.java
+++ IMS-Desktop/src/main/java/io/amecodelabs/ims/App.java
@@ -1,11 +1,36 @@
 package io.amecodelabs.ims;
 
-import io.amecodelabs.ims.view.MainView;
+import io.amecodelabs.ims.view.base.BuildWindowDirector;
+import io.amecodelabs.ims.view.context.ApplicationContext;
+import io.amecodelabs.ims.view.context.Session;
+import io.amecodelabs.ims.view.loginstage.LoginBuildable;
+import io.amecodelabs.ims.view.mainstage.MainBuildable;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
+	
+	public static void main(String[] args) throws Exception {
+		launch(args);
+	}
+	
+	@Override
+	public void init() throws Exception {
+		ApplicationContext context = ApplicationContext.getInstance();
+		context.addAttribute("host-services", getHostServices());
+		
+		BuildWindowDirector
+			.getDirector()
+				.prepare("login", new LoginBuildable())
+				.prepare("main", new MainBuildable());
+	}
 
-	public static void main(String[] args) {
-		MainView.run(args);
+	@Override
+	public void start(Stage stage) throws Exception {
+		if ( Session.isSession() ) 
+			BuildWindowDirector.getDirector().create("main");
+		else
+			BuildWindowDirector.getDirector().create("login");
 	}
 
 }
