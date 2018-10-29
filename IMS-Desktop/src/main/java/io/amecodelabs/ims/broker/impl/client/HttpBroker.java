af3c77ab6e99b2ac57038e911c14c6bfadba48ed
Alan M.E <alanmarquez@outlook.com>
2018-10-28 19:15:44 -0500

    fixed bug: POST request

--- IMS-Desktop/src/main/java/io/amecodelabs/ims/broker/impl/client/HttpBroker.java
+++ IMS-Desktop/src/main/java/io/amecodelabs/ims/broker/impl/client/HttpBroker.java
@@ -3,11 +3,9 @@ package io.amecodelabs.ims.broker.impl.client;
 import io.amecodelabs.ims.broker.client.HttpConnect;
 
 public class HttpBroker {
-	private static HttpConnect connect;
 	
 	public static HttpConnect getHttpConnect() {
-		if(connect == null) connect = new DefaultHttpConnect();
-		return connect;
+		return  new DefaultHttpConnect();
 	}
 	
 }
