e0aef0f1e5a439190c76ba1e9c6620178c14d864
Alan M.E <alanmarquez@outlook.com>
2018-11-09 11:37:07 -0500

    Organized code #1

--- IMS-Desktop/src/main/java/io/amecodelabs/ims/view/providerstage/PresenterProvidersImpl.java
+++ IMS-Desktop/src/main/java/io/amecodelabs/ims/view/providerstage/PresenterProvidersImpl.java
@@ -15,22 +15,21 @@ public class PresenterProvidersImpl implements PresenterProvidersView<ContentVal
 	@Override
 	public void saveProvider(ContentValues providers) {
 		this.providerView.showRegisterProgress();
-		service.createProvider(
-				providers, 
-				(response, document) -> {
-					this.providerView.hiddenRegisterProgress();
-					if(response.getValueInteger("ok") == 1) {
-						this.providerView.showMessage("Success", "Provider has been successfully added");
-						this.providerView.update(document);
-						this.providerView.clearForm();
-					} else {
-						this.providerView.showMessage("Error", "Provider could not be added");
-					}
-				},
-				(err) -> {
-					this.providerView.hiddenRegisterProgress();
-					this.providerView.showMessage("Error", "Connect failed or interrupted");
+		service.createProvider(providers, 
+			(response, document) -> {
+				this.providerView.hiddenRegisterProgress();
+				if(response.getValueInteger("ok") == 1) {
+					this.providerView.showMessage("Success", "Provider has been successfully added");
+					this.providerView.update(document);
+					this.providerView.clearForm();
+				} else {
+					this.providerView.showMessage("Error", "Provider could not be added");
 				}
+			},
+			(err) -> {
+				this.providerView.hiddenRegisterProgress();
+				this.providerView.showMessage("Error", "Connect failed or interrupted");
+			}
 		);
 	}
 
@@ -43,59 +42,57 @@ public class PresenterProvidersImpl implements PresenterProvidersView<ContentVal
 	public void getProviders() {
 		this.providerView.showLoadProgress();
 		service.getProviders(
-				(response) -> {
-					this.providerView.hiddenloadProgress();
+			(response) -> {
+				this.providerView.hiddenloadProgress();
 					if(response.getValueInteger("ok") == 1) {
 						this.providerView.loadProviders(response.getArrayContentValues("response"));
 					} else {
 						this.providerView.showMessage("Error", "Providers have not been downloaded");
 					}
 				}, 
-				(err) -> {
-					this.providerView.hiddenloadProgress();
-					this.providerView.showMessage("Error", "Connect failed or interrupted");
-				}
+			(err) -> {
+				this.providerView.hiddenloadProgress();
+				this.providerView.showMessage("Error", "Connect failed or interrupted");
+			}
 		);
 	}
 	
 	@Override
 	public void editProvider(ContentValues updateProvider) {
 		this.providerView.showLoadProgress();
-		service.EditProvider(
-				updateProvider, 
-				(response) -> {
-					this.providerView.hiddenloadProgress();
-					if(response.getValueInteger("ok") == 1) {
-						this.providerView.showMessage("Update", "Provider has been updated");
-					} else {
-						this.providerView.showMessage("Error", "Provider has not been updated");
-					}
-				}, 
-				(err) -> {
-					this.providerView.hiddenloadProgress();
-					this.providerView.showMessage("Error", "Connect failed or interrupted");
+		service.EditProvider(updateProvider, 
+			(response) -> {
+				this.providerView.hiddenloadProgress();
+				if(response.getValueInteger("ok") == 1) {
+					this.providerView.showMessage("Update", "Provider has been updated");
+				} else {
+					this.providerView.showMessage("Error", "Provider has not been updated");
 				}
+			}, 
+			(err) -> {
+				this.providerView.hiddenloadProgress();
+				this.providerView.showMessage("Error", "Connect failed or interrupted");
+			}
 		);
 	}
 
 	@Override
 	public void deleteProvider(String id) {
 		this.providerView.showLoadProgress();
-		service.deleteProviders(
-				id, 
-				(response) -> {
-					this.providerView.hiddenloadProgress();
-					if(response.getValueInteger("ok") == 1) {
-						this.providerView.showMessage("Update", "Provider has been removed");
-						this.providerView.remove(id);
-					} else {
-						this.providerView.showMessage("Error", "Provider has not been removed");
-					}
-				},
-				(err) -> {
-					this.providerView.hiddenloadProgress();
-					this.providerView.showMessage("Error", "Connect failed or interrupted");
+		service.deleteProviders(id, 
+			(response) -> {
+				this.providerView.hiddenloadProgress();
+				if(response.getValueInteger("ok") == 1) {
+					this.providerView.showMessage("Update", "Provider has been removed");
+					this.providerView.remove(id);
+				} else {
+					this.providerView.showMessage("Error", "Provider has not been removed");
 				}
+			},
+			(err) -> {
+				this.providerView.hiddenloadProgress();
+				this.providerView.showMessage("Error", "Connect failed or interrupted");
+			}
 		);
 	}
 
