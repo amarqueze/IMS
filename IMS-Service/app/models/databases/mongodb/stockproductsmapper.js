9c70b674bd79606488830c5e1223d298098c12ca
Alan M.E <alanmarquez@outlook.com>
2019-02-10 19:09:20 -0500

    sorted stock

--- IMS-Service/app/models/databases/mongodb/stockproductsmapper.js
+++ IMS-Service/app/models/databases/mongodb/stockproductsmapper.js
@@ -1,4 +1,5 @@
 module.exports = function(objectmapper) {
+    var connector = objectmapper.getConnector();
     var collectionName = "stockproducts";
 
     return {
@@ -8,9 +9,24 @@ module.exports = function(objectmapper) {
         update(params, updatedDocument, success, fail) {
             objectmapper.update(collectionName, params, {$set: updatedDocument}, success, fail);
         },
-        find(params, success, fail) {
-            params.date = new RegExp(params.date + ".*", "g");
-            objectmapper.find(collectionName, params, success, fail);
+        find(params, skip, success, fail) {
+            if(params.date)
+                params.date = new RegExp(params.date + ".*", "g");
+            var sort = { date: -1 };
+
+            connector.open(
+                (dbo) => {
+                    dbo.collection(collectionName).find(params)
+                    .skip(skip)
+                    .sort(sort)
+                    .limit(11).toArray(function(err, result) {
+                        if (err) fail(err);
+                        else success(result);
+                        connector.close();
+                    });
+                },
+                (err) => fail(err)
+            );
         },
         delete(params, success, fail) {
             objectmapper.delete(collectionName, params, success, fail);
